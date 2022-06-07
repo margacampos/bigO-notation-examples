5f35395fec427177e283503db9b90c2d3c9233db
margacampos <margacampos@hotmail.es>
2022-06-07 09:25:36 +0100

    Binary search return false when target is not found

--- binary-search.js
+++ binary-search.js
@@ -1,13 +1,16 @@
 const arr = [1,4,5,7,9,24];
 let start = 0;
 let end = arr.length - 1;
-let target = 24;
+let targetTrue = 4;
+let targetFalse = 25;
 
 const binarySearch = ( arr, start, end, target ) => {
     let midIndex = Math.floor( ( start + end ) / 2 );
     if ( arr[midIndex] === target )return true;
-    if ( arr[midIndex] < target )return binarySearch(arr, midIndex + 1, end, target );
-    else binarySearch( arr, start, midIndex - 1, target );
+    else if (  end - start === 0 ) return false;
+    if ( arr[midIndex] < target ) return binarySearch(arr, midIndex + 1, end, target );
+    else return binarySearch( arr, start, midIndex - 1, target );
 };
 
-console.log(binarySearch( arr, start, end, target ));
+console.log(binarySearch( arr, start, end, targetTrue ));
+console.log(binarySearch( arr, start, end, targetFalse ));
